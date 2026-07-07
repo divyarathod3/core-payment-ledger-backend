7f0eab293e1d2b585ec6b87d965375566413701f
Divya Rathod <rathoddivya3002@gmail.com>
2026-07-07 22:54:49 +0530

    Refactored User entity into correct package structure

---
+++ src/main/java/com/company/wallet/entity/Wallet.java
@@ -0,0 +1,23 @@
+package com.company.wallet.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "wallets")
+@Data
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
